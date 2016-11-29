424a8be4c409e539375ed6020af5a237b36e263e
Pierre Toroyan <p.toroyan@gmail.com>
2016-11-29 14:31:54 -0800

    NoteTableViewCell
    - xcode warning

---
+++ NoteTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  NoteTableViewCell.swift
+//  NotifyMe
+//
+//  Created by Pierre on 11/29/16.
+//  Copyright © 2016 Pierre. All rights reserved.
+//
+
+import UIKit
+
+class NoteTableViewCell: UITableViewCell {
+
+    
+    //MARK: IBOulets
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
