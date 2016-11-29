73e9a94a9ea67957432d31d1fd9a801b243196de
Pierre Toroyan <p.toroyan@gmail.com>
2016-11-29 14:30:25 -0800

    NoteListViewController
    -commit due to xcode warning

--- NoteListViewController.swift
+++ NoteListViewController.swift
@@ -11,29 +11,56 @@ import Firebase
 
 class NoteListViewController: UIViewController {
 
+    //MARK: Properties
+    var notes: [Note] = []
     
+    
+    //MARK: IBOutlets
+    @IBOutlet weak var tableView: UITableView!
+    
+	
     //MARK: View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-		
-		checkForCurrentuser()
     }
     
-    
-    //MARK: IBActions
-    @IBAction func logoutButtonTapped(_ sender: Any) {
-        try! FIRAuth.auth()?.signOut()
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
-        checkForCurrentuser()
-    }
-    
-    
-    //MARK: Helper Functions
-    func checkForCurrentuser() {
-        if let user = FIRAuth.auth()?.currentUser {
-            print("\(user) signed in")
+        if let currentUser = UserController.sharedController.currentUser {
+        	loadNotesForUser(user: currentUser)
         } else {
             performSegue(withIdentifier: "toSignupLoginView", sender: self)
         }
     }
+    
+    
+    //MARK: Builder Functions
+    func loadNotesForUser(user: User) {
+        NoteController.fetchNotesForUser(user: user, completion: { (notes) -> Void in
+            if let notes = notes {
+                self.notes = notes
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            } else {
+                print("no notes to display")
+            }
+        })
+    }
+    
+}
+
+
+extension NoteListViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 }
