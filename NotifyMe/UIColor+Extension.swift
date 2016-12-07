0dc81d00fd2fab9e4abc22b8cfde7af0c8774b60
Pierre Toroyan <p.toroyan@gmail.com>
2016-12-07 14:23:48 -0800

    UIColor+Extension
    - created an extension on UIColor to hold my custom colors

---
+++ NotifyMe/UIColor+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor+Extension.swift
+//  NotifyMe
+//
+//  Created by Pierre on 11/29/16.
+//  Copyright © 2016 Pierre. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    static func purpleThemeColor() -> UIColor {
+		return UIColor(red: 206/255.0, green: 149/255.0, blue: 255/255.0, alpha: 1.0)
+    }
+}
