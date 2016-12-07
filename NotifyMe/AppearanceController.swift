777f5bd78bea85dfa5e82714264baf8a2479515e
Pierre Toroyan <p.toroyan@gmail.com>
2016-12-07 14:23:18 -0800

    AppearanceController created
    - created AppearanceController to have UI in one place

---
+++ NotifyMe/AppearanceController.swift
@@ -0,0 +1,20 @@
+//
+//  AppearanceController.swift
+//  NotifyMe
+//
+//  Created by Pierre on 11/29/16.
+//  Copyright © 2016 Pierre. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AppearanceController {
+    
+    static func initializeAppearance() {
+        UINavigationBar.appearance().barStyle = .black
+        UINavigationBar.appearance().barTintColor = .purpleThemeColor()
+        UINavigationBar.appearance().tintColor = .white
+    }
+    
+}
