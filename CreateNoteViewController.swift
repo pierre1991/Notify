996a05e4e2f733116f64408b8d2e8045f51a9537
Pierre Toroyan <p.toroyan@gmail.com>
2016-12-09 10:16:27 -0800

    Get All Users
    - created getAllUsers with a filter to exclude currentUser

--- CreateNoteViewController.swift
+++ CreateNoteViewController.swift
@@ -11,6 +11,7 @@ import Firebase
 
 class CreateNoteViewController: UIViewController {
     
+    
     //MARK: Properties
     let currentUser = FIRAuth.auth()?.currentUser
     var allUsers: [User]?
@@ -28,23 +29,53 @@ class CreateNoteViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        UserController.fetchAllUsers { (user) in
-            guard let user = user else {return}
-            
-            self.allUsers = user.filter({$0.identifier != UserController.sharedController.currentUser.identifier})
-            
-            DispatchQueue.main.async {
-				self.userCollectionView.reloadData()
+        setupCollectionView()
+        
+        getAllUsers { (users) in
+            if let users = users {
+                self.allUsers = users.filter({$0.identifier != FIRAuth.auth()?.currentUser?.uid})
+    
+                DispatchQueue.main.async {
+                    self.userCollectionView.reloadData()
+                }
+            } else {
+                self.allUsers = []
             }
         }
     }
     
     
+    //MARK: Touches
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        noteTitleTextField.resignFirstResponder()
+        noteBodyTextView.resignFirstResponder()
+    }
+    
+    
     //MARK: IBActions
     @IBAction func createNoteButtonTapped(_ sender: Any) {
-        guard let navigationController = navigationController else {return}
-        navigationController.popToRootViewController(animated: true)
-	}
+        guard let title = noteTitleTextField.text, let body = noteBodyTextView.text else {return}
+        
+        NoteController.createNote(title: title, text: body, identifier: (FIRAuth.auth()?.currentUser?.uid)!, users: selectedUsersForNote, completion: { (success, note) -> Void in
+            if note != nil {
+                guard let navigationController = navigationController else {return}
+                navigationController.popToRootViewController(animated: true)
+                
+                self.selectedUsersForNote = (note?.users)!
+            } else {
+                //TODO: present user with alert
+                
+                print("unable to create note")
+            }
+        })
+    }
+    
+    
+    func getAllUsers(completion: @escaping (_ users: [User]?) -> Void) {
+        UserController.fetchAllUsers { (users) in
+            completion(users)
+        }
+    }
 
 }
 
@@ -54,7 +85,7 @@ extension CreateNoteViewController: UICollectionViewDelegate, UICollectionViewDa
         if let allUsers = allUsers {
             return allUsers.count
         } else {
-            return 12
+            return 0
         }
     }
     
@@ -75,12 +106,26 @@ extension CreateNoteViewController: UICollectionViewDelegate, UICollectionViewDa
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let cell = collectionView.cellForItem(at: indexPath)
+        
         cell?.contentView.backgroundColor = .blue
         
         if let users = allUsers {
             let user = users[indexPath.item]
             self.selectedUsersForNote.append(user)
         }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.contentView.backgroundColor = nil
+        
+        self.selectedUsersForNote.removeLast()
+    }
+    
+    func setupCollectionView() {
+        userCollectionView.allowsMultipleSelection = true
+        
+        
     }
     
 }
