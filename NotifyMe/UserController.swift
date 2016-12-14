d0f69e1deddce6206bd62ba6b72d373054140b70
Pierre Toroyan <p.toroyan@gmail.com>
2016-12-14 15:46:54 -0800

    UserController.sharedController.currentUser
    - fixed the problem of saving the currentUser to UserDefaults

--- NotifyMe/UserController.swift
+++ NotifyMe/UserController.swift
@@ -16,11 +16,10 @@ class UserController {
     static let sharedController = UserController()
 
     
+    
     var currentUser: User! {
         get {
-            guard let uid = FIRAuth.auth()?.currentUser?.uid,
-            	let userDefaultsValue = UserDefaults.standard.value(forKey: kUser),
-                let userDictionary = userDefaultsValue as? [String:AnyObject] else {return nil}
+            guard let uid = FIRAuth.auth()?.currentUser?.uid, let userDictionary = UserDefaults.standard.value(forKey: kUser) as? [String:AnyObject] else {return nil}
             
             return User(json: userDictionary, identifier: uid)
         } set {
@@ -33,8 +32,6 @@ class UserController {
             }
         }
     }
-    
-
     
     
     static func userForIdentifier(identifier: String, completion: @escaping (_ user: User?) -> Void) {
@@ -50,14 +47,16 @@ class UserController {
     
     
     static func createUser(email: String, password: String, imageEndpoint: String? = nil, completion: @escaping (_ success: Bool, _ user: User?) -> Void) {
-        FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
+       	FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
             if error == nil {
                 guard let identifier = user?.uid else {return}
                 
                 var user = User(email: email, imageEndpoint: imageEndpoint, identifier: identifier)
                 user.save()
                 
-                completion(true, user)
+                authenticateUser(email: email, password: password, completion: { (success, user) in
+                    completion(success, user)
+                })
             } else {
                 print("unable to create user")
                 completion(false, nil)
@@ -66,19 +65,29 @@ class UserController {
     }
     
     
-    static func authenticateUser(email: String, password: String, completion: @escaping (_ success: Bool) -> Void) {
+    static func authenticateUser(email: String, password: String, completion: @escaping (_ success: Bool, _ user: User?) -> Void) {
         FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
+            guard let user = user else { return }
+            
             if error == nil {
-                completion(true)
+                print("\(user.uid) signed in succesfully")
+                
+                UserController.userForIdentifier(identifier: user.uid, completion: { (user) in
+                    if let user = user {
+                        UserController.sharedController.currentUser = user
+                    }
+                  
+                    completion(true, user)
+                })
             } else {
                 print("unalbe to sign in")
-                completion(false)
+                completion(false, nil)
             }
         })
     }
     
     
-    static func fetchAllUsers(completion: @escaping (_ user: [User]?) -> Void) {
+    static func fetchAllUsers(completion: @escaping (_ user: [User]) -> Void) {
         FirebaseController.dataAtEndpoint(endpoint: "users") { (data) in
             if let data = data as? [String:AnyObject] {
                 let users = data.flatMap({User(json: $0.1 as! [String:AnyObject], identifier: $0.0)})
@@ -88,7 +97,7 @@ class UserController {
             }
         }
     }
-    
 
     
+
 }
