c1115bb172e20ed1941d26fc1b938eabf621f5f1
Pierre Toroyan <ghostcatapp@gmail.com>
2016-12-29 10:04:03 -0800

    Report model
    - create model class for Reporting user picture

---
+++ NotifyMe/Report.swift
@@ -0,0 +1,41 @@
+//
+//  Report.swift
+//  NotifyMe
+//
+//  Created by Pierre on 12/28/16.
+//  Copyright © 2016 Pierre. All rights reserved.
+//
+
+import Foundation
+
+class Report {
+    
+    fileprivate let kReporterID = "reporterID"
+    fileprivate let kUserID = "userID"
+    
+    let reporterID: String
+    let userID: String
+    var identifier: String?
+    var endpoint: String {
+        return "reports"
+    }
+    
+    var jsonValue: [String: AnyObject] {
+        return [kReporterID: reporterID as AnyObject, kUserID: userID as AnyObject]
+    }
+    
+    init(reporterID: String, userID: String) {
+        self.reporterID = reporterID
+        self.userID = userID
+    }
+    
+    init?(json jsonValue: [String: AnyObject], identifier: String) {
+        guard let reporterID = jsonValue[kReporterID] as? String, let userID = jsonValue[kUserID] as? String else { return nil }
+        
+        self.reporterID = reporterID
+        self.userID = userID
+        
+        self.identifier = identifier
+    }
+    
+}
