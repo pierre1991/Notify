425547cea82831d62a759d3edce68a756ece119e
Pierre Toroyan <p.toroyan@gmail.com>
2017-01-10 13:10:58 -0800

    Extension+UIView
    - shake() to shake UIView for incorrect password or empty field if no information is inputted

---
+++ NotifyMe/Extension+UIView.swift
@@ -0,0 +1,24 @@
+//
+//  Extension+UIView.swift
+//  NotifyMe
+//
+//  Created by Pierre on 1/10/17.
+//  Copyright © 2017 Pierre. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+
+    func shake() {
+        let animation = CABasicAnimation(keyPath: "position")
+        animation.duration = 0.07
+        animation.repeatCount = 4
+        animation.autoreverses = true
+        animation.fromValue = NSValue(cgPoint: CGPoint(x: self.center.x - 10, y: self.center.y))
+        animation.toValue = NSValue(cgPoint: CGPoint(x: self.center.x + 10, y: self.center.y))
+
+        self.layer.add(animation, forKey: "position")
+    }
+}
