409a27f362ce32759f59f9fb5b531487001a2e48
Pierre Toroyan <p.toroyan@gmail.com>
2017-01-29 23:14:02 -0800

    Notes
    - added notest to allow for easier understanding of methods

--- NotifyMe/FirebaseController.swift
+++ NotifyMe/FirebaseController.swift
@@ -24,16 +24,20 @@ class FirebaseController {
             }
         })
     }
-
 }
 
 protocol FirebaseType {
-    
+    // Identifier will be an optional string (assigned when object is created in firebase .childByAutoID())
     var identifier: String? { get set }
+    
+    // Endpoint is a required string used to define the path that must be traversed in firebase for access
     var endpoint: String { get }
+
+    // JSON Value is a computed dictionary that will be used to store the various attributes of model objects in firebase
     var jsonValue: [String: AnyObject] { get }
     
-    init?(json: [String: AnyObject], identifier: String)
+    // Failable initializer that utilizes a JSON formatted dictionary to initialize a partiular model object. Requires identifier for initialization
+    init?(jsonValue: [String: AnyObject], identifier: String)
     
     mutating func save()
     func delete()
@@ -42,19 +46,21 @@ protocol FirebaseType {
 
 extension FirebaseType {
     
+    // All objects conforming to FirebaseType will implement the following save function. This implementation checks for an identifier, if identifier is present the identifier is passed into childByAppendingPath. If not, a new ID is generated. The Firebase location is then updated with the jsonValue of the respective path
     mutating func save() {
         var endpointBase: FIRDatabaseReference
         
         if let identifier = identifier {
-            endpointBase = FIRDatabase.database().reference().child(endpoint).child(identifier)
+            endpointBase = FirebaseController.base.child(endpoint).child(identifier)
         } else {
-            endpointBase = FIRDatabase.database().reference().child(endpoint).childByAutoId()
+            endpointBase = FirebaseController.base.child(endpoint).childByAutoId()
             self.identifier = endpointBase.key
         }
         
         endpointBase.updateChildValues(self.jsonValue)
     }
     
+    // Uses endpoint and identifier to construct a path and remove jsonValue at the path's value
     func delete() {
         if let identifier = self.identifier {
             let endpointBase = FIRDatabase.database().reference().child(endpoint).child(identifier)
