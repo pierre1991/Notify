20dc30f62a7b2ab9db148fc3896e42f6de9e9b6b
Pierre Toroyan <p.toroyan@gmail.com>
2017-01-30 15:01:05 -0800

    User model update
    - updated the User model to accomodate for a “var username: String” property
    
    - ofcourse handles all the necessary init’s also

--- NotifyMe/User.swift
+++ NotifyMe/User.swift
@@ -11,10 +11,12 @@ import Firebase
 
 class User: FirebaseType {
     
+    private let kUsername = "username"
     private let kEmail = "email"
     private let kImageEndpoint = "imageEndpoint"
     private let kNoteId = "noteId"
     
+    var username: String
     var email: String
     var imageEndpoint: String?
     var notes: [Note] = []
@@ -31,7 +33,7 @@ class User: FirebaseType {
     }
     
     var jsonValue: [String: AnyObject] {
-        var json: [String: AnyObject] = [kEmail: email as AnyObject, kNoteId: noteId as AnyObject]
+        var json: [String: AnyObject] = [kUsername: username as AnyObject, kEmail: email as AnyObject, kNoteId: noteId as AnyObject]
         
         if let imageEndpoint = imageEndpoint {
             json.updateValue(imageEndpoint as AnyObject, forKey: kImageEndpoint)
@@ -41,15 +43,17 @@ class User: FirebaseType {
     }
     
     //Init
-    init(email: String, imageEndpoint: String? = nil, identifier: String) {
+    init(username: String, email: String, imageEndpoint: String? = nil, identifier: String) {
+        self.username = username
         self.email = email
         self.imageEndpoint = imageEndpoint
         self.identifier = identifier
     }
     
     required init?(jsonValue: [String: AnyObject], identifier: String) {
-        guard let email = jsonValue[kEmail] as? String, let imageEndpoint = jsonValue[kImageEndpoint] as? String else { return nil }
+        guard let username = jsonValue[kUsername] as? String, let email = jsonValue[kEmail] as? String, let imageEndpoint = jsonValue[kImageEndpoint] as? String else { return nil }
         
+        self.username = username
         self.email = email
         self.imageEndpoint = imageEndpoint
         self.identifier = identifier
