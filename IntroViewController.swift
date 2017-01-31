db6337c41a138f4ff42dc1be7e7396e11920b549
Pierre Toroyan <p.toroyan@gmail.com>
2017-01-31 13:06:36 -0800

    IntroViewController
    - container view for implementing IntroPageViewController
    - also holds signup_button view

---
+++ IntroViewController.swift
@@ -0,0 +1,48 @@
+//
+//  IntroViewController.swift
+//  NotifyMe
+//
+//  Created by Pierre on 1/24/17.
+//  Copyright © 2017 Pierre. All rights reserved.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+	//MARK: IBOutlets
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    
+    //MARK: View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.navigationBar.isHidden = true 
+    }
+    
+    //MARK: Status Bar
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+    @IBAction func signupButtonTapped(_ sender: Any) {
+//        if let signupViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "LoginSignupViewController") as? LoginSignupViewController {
+//            present(signupViewController, animated: true, completion: nil)
+//        }
+    }
+}
+
+extension IntroViewController: IntroPageControlDelegate {
+    
+    func introPageViewControllerCount(introPageViewController: IntroPageViewController, didUpdatePageCount count: Int) {
+        pageControl.numberOfPages = count
+        
+    }
+    
+    func pageViewControllerIndex(introPageViewController: IntroPageViewController, didUpdatePageIndex index: Int) {
+        pageControl.currentPage = index
+    }
+    
+}
