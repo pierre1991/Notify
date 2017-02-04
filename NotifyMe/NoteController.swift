96295346a3b745f21f3fb01130796b63cc857c9a
Pierre Toroyan <p.toroyan@gmail.com>
2017-02-03 23:27:47 -0800

    completion -> completionHandler (name change)

--- NotifyMe/NoteController.swift
+++ NotifyMe/NoteController.swift
@@ -34,7 +34,7 @@ class NoteController {
     
     static let sharedController = NoteController()
     
-	static func fetchNotesForUser(_ user: User, completion:@escaping (_ notes: [Note]?) -> Void) {
+	static func fetchNotesForUser(_ user: User, completionHandler: @escaping (_ notes: [Note]?) -> Void) {
         var allNotes: [Note] = []
         let dispatchGroup = DispatchGroup()
         
@@ -49,93 +49,88 @@ class NoteController {
         
         dispatchGroup.notify(queue: DispatchQueue.main) { () -> Void in
             let orderedNotes = orderNotes(note: allNotes)
-            completion(orderedNotes)
+            completionHandler(orderedNotes)
         }
     }
     
     
-    static func notesForUser(_ user: User, completion:@escaping (_ notes: [Note]?) -> Void) {
+    static func notesForUser(_ user: User, completionHandler: @escaping (_ notes: [Note]?) -> Void) {
         FirebaseController.base.child("/users/\(user.identifier!)").child("noteId").observeSingleEvent(of: .value, with: {snapshot in
             if let noteIdArray = snapshot.value as? [String] {
                 var noteArray: [Note] = []
                 
                 for noteId in noteIdArray {
-                    notesFromIdentifier(identifier: noteId, completion: { (notes) -> Void in
+                    notesFromIdentifier(identifier: noteId, completionHandler: { (notes) -> Void in
                         if let note = notes {
                             noteArray.append(note)
                         }
 
                         if noteId == noteIdArray.last {
                             let orderedNotes = orderNotes(note: noteArray)
-                            completion(orderedNotes)
+                            completionHandler(orderedNotes)
                         }
                     })
                 }
             } else {
-                completion(nil)
+                completionHandler(nil)
             }
         })
     }
 
     
-    static func notesFromIdentifier(identifier: String, completion: @escaping (_ note: Note?) -> Void) {
+    static func notesFromIdentifier(identifier: String, completionHandler: @escaping (_ note: Note?) -> Void) {
         FirebaseController.dataAtEndpoint(endpoint: "/notes/\(identifier)", completion: {(data) -> Void in
             if let data = data as? [String: AnyObject] {
                 let note = Note(jsonValue: data, identifier: identifier)
                 
-                completion(note)
+                completionHandler(note)
             } else {
-                completion(nil)
+                completionHandler(nil)
             }
         })
     }
     
     
-    static func fetchUsersForNote(note: Note, completion: @escaping (_ user: [User]?) -> Void) {
+    static func fetchUsersForNote(note: Note, completionHandler: @escaping (_ user: [User]?) -> Void) {
         FirebaseController.base.child("/notes/\(note.identifier!)").child("userIds").observeSingleEvent(of: .value, with: {snapshot in
             if let userIdArray = snapshot.value as? [String] {
                 var userArray: [User] = []
                 
                 for userId in userIdArray {
-                    UserController.userForIdentifier(identifier: userId, completion: { (user) in
+                    UserController.userForIdentifier(identifier: userId, completionHandler: { (user) in
                         if let user = user {
                             userArray.append(user)
-                            completion(userArray)
+                            completionHandler(userArray)
                         }
                     })
                 }
             } else {
-                completion([])
+                completionHandler([])
             }
         })
     }
     
     //MARK: Create
-    static func createNote(title: String, text: String, identifier: String, users: [User], completion: (_ success: Bool, _ note: Note?) -> Void) {
-        var note = Note(title: title, text: text, identifier: identifier, users: users)
+    static func createNote(title: String, text: String, users: [User], completionHandler: (_ note: Note?) -> Void) {
+        var note = Note(title: title, text: text, users: users)
         note.save()
         
-        guard let noteId = note.identifier else {
-            completion(false, nil)
-            return
-        }
+        guard let noteID = note.identifier else { completionHandler(nil); return }
         
-        var user = UserController.sharedController.currentUser!
-        user.noteId.insert(noteId, at: 0)
-        user.save()
+        var user = UserController.sharedController.currentUser
+        user?.noteId.append(noteID)
+        user?.save()
         
         for var user in users {
-            user.noteId.insert(noteId, at: 0)
+            user.noteId.append(noteID)
             user.save()
         }
         
-        completion(true, note)
+        completionHandler(note)
     }
  
-    //MARK: Order
-    static func orderNotes(note: [Note]) -> [Note] {
-        return note.sorted(by: {$0.0.identifier > $0.1.identifier})
-    }
+    //MARK: Update
+    
     
     //MARK: Save
     static func saveNote(note: Note) {
@@ -146,6 +141,11 @@ class NoteController {
     // MARK: Delete
     static func deleteNote(note: Note) {
         note.delete()
+    }
+    
+    //MARK: Order
+    static func orderNotes(note: [Note]) -> [Note] {
+        return note.sorted(by: {$0.0.identifier > $0.1.identifier})
     }
     
 }
