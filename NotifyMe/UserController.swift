2353ef7a1c287759dab9e476ba11ed5eac5a1a34
Pierre Toroyan <p.toroyan@gmail.com>
2017-02-04 09:08:56 -0800

    completion -> completionHandler

--- NotifyMe/UserController.swift
+++ NotifyMe/UserController.swift
@@ -15,13 +15,11 @@ class UserController {
     
     static let sharedController = UserController()
 
-    
-    
     var currentUser: User! {
         get {
             guard let uid = FIRAuth.auth()?.currentUser?.uid, let userDictionary = UserDefaults.standard.value(forKey: kUser) as? [String:AnyObject] else {return nil}
             
-            return User(json: userDictionary, identifier: uid)
+            return User(jsonValue: userDictionary, identifier: uid)
         } set {
             if let newValue = newValue {
                 UserDefaults.standard.setValue(newValue.jsonValue, forKey: kUser)
@@ -35,71 +33,81 @@ class UserController {
     
     
     // Create User
-    static func createUser(email: String, password: String, imageEndpoint: String? = nil, completion: @escaping (_ success: Bool, _ user: User?) -> Void) {
+    static func createUser(username: String, email: String, password: String, imageEndpoint: String? = nil, completionHandler: @escaping (_ success: Bool, _ user: User?) -> Void) {
         FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) -> Void in
             if error == nil {
                 guard let identifier = user?.uid else { return }
                 
-                var user = User(email: email, imageEndpoint: imageEndpoint, identifier: identifier)
+                var user = User(username: username, email: email, imageEndpoint: imageEndpoint, identifier: identifier)
                 user.save()
                 
-                authenticateUser(email: email, password: password, completion: { (success, user) in
-                    completion(true, user)
+                authenticateUser(username: username,email: email, password: password, completionHandler: { (success, user) in
+                    completionHandler(true, user)
                 })
             } else {
                 print("unable to create user")
-                completion(false, nil)
+                completionHandler(false, nil)
         	}
         })
     }
     
     
     // Authenticate User
-    static func authenticateUser(email: String, password: String, completion: @escaping (_ success: Bool, _ user: User?) -> Void) {
+    static func authenticateUser(username: String, email: String, password: String, completionHandler: @escaping (_ success: Bool, _ user: User?) -> Void) {
         FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) -> Void in
             if error != nil {
                 print("Unsuccessful login attempt.")
                 
-                completion(false, nil)
+                completionHandler(false, nil)
             } else {
                 guard let user = user else { return }
                 
                 print("User ID: \(user.uid) authenticated successfully.")
                 
-                UserController.userForIdentifier(identifier: user.uid, completion: { (user) -> Void in
+                UserController.userForIdentifier(identifier: user.uid, completionHandler: { (user) -> Void in
                     if let user = user {
                         sharedController.currentUser = user
                     }
                     
-                    completion(true, user)
+                    completionHandler(true, user)
                 })
             }
         })
     }
     
     
+    // Update User Image
+//    static func updateUsersImage(identifier: String, image: UIImage, completion: (_ identifier: String?) -> Void) {
+//        FirebaseController.dataAtEndpoint(endpoint: "image/\(identifier)") { (data) in
+//            if let data = data as? String {
+//                
+//            }
+//        }
+//    }
+    
+    
     // Return User from Identifier
-    static func userForIdentifier(identifier: String, completion: @escaping (_ user: User?) -> Void) {
+    static func userForIdentifier(identifier: String, completionHandler: @escaping (_ user: User?) -> Void) {
         FirebaseController.dataAtEndpoint(endpoint: "users/\(identifier)", completion: {(data) -> Void in
-            if let json = data as? [String:AnyObject] {
-                let user = User(json: json, identifier: identifier)
+            if let json = data as? [String: AnyObject] {
+                let user = User(jsonValue: json, identifier: identifier)
                 
-                completion(user)
+                completionHandler(user)
             } else {
-                completion(nil)
+                completionHandler(nil)
             }
         })
     }
     
     
     // Get all Users of App
-    static func fetchAllUsers(completion: @escaping (_ user: [User]) -> Void) {
+    static func fetchAllUsers(completionHandler: @escaping (_ user: [User]) -> Void) {
         FirebaseController.dataAtEndpoint(endpoint: "users") { (data) in
             if let data = data as? [String:AnyObject] {
-                let users = data.flatMap({User(json: $0.1 as! [String:AnyObject], identifier: $0.0)})
-                completion(users)
+                let users = data.flatMap({User(jsonValue: $0.1 as! [String: AnyObject], identifier: $0.0)})
+                completionHandler(users)
             } else {
-                completion([])
+                completionHandler([])
             }
         }
     }
